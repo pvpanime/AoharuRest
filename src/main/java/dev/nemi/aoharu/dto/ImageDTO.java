97ed4f6517ae7cc4d5b6470f737981625137bd08
pvpanime <nemisprint@gmail.com>
2025-01-20 17:41:03 +0900

    :)

--- src/main/java/dev/nemi/aoharu/dto/ImageDTO.java
+++ src/main/java/dev/nemi/aoharu/dto/ImageDTO.java
@@ -1,5 +1,6 @@
 package dev.nemi.aoharu.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,10 +15,12 @@ public class ImageDTO {
   private String name;
   private int ordinal;
 
+  @JsonProperty("src")
   public String getSrc() {
     return String.format("/i/%s_%s", id, name);
   }
 
+  @JsonProperty("thumbnail")
   public String getThumbnail() {
     return String.format("/th/%s_%s", id, name);
   }
